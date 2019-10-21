dad728c9e9e117f83ebf67a4d76b0e2c1fcaebd1
Leandro Biciato <leandrobiciato58@gmail.com>
2019-10-21 15:41:20 -0300

    Updates

---
+++ precache-manifest.8bee4636d5d06cd00deb34942c53188e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150491805119170974eac047a48122d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d895407fe2521e220e87",
+    "url": "/static/css/main.a125415f.chunk.css"
+  },
+  {
+    "revision": "2f9161fb42bd752443a4",
+    "url": "/static/js/2.289b0f6d.chunk.js"
+  },
+  {
+    "revision": "d895407fe2521e220e87",
+    "url": "/static/js/main.1d933ee4.chunk.js"
+  },
+  {
+    "revision": "b300f7aec494cca148c5",
+    "url": "/static/js/runtime-main.806b1a07.js"
+  }
+]);
